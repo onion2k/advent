851eafa02135b4c370eea8fb6e09cebfcab090c7
Chris Neale <chris@komododigital.co.uk>
2017-09-26 08:44:35 +0100

    Clickable doors

--- src/Door.js
+++ src/Door.js
@@ -3,11 +3,25 @@ import './Door.css';
 
 class Door extends Component {
 
+    constructor(){
+        super();
+        this.state = { open: false };
+        this.onClick = this.onClick.bind(this);
+    }
+
+    onClick(){
+        this.setState({ open: !this.state.open });
+    }
+
     render() {
 
         return (
-            <div className={'Door'+(this.props.number==="24"?' double':'')+(this.props.number==="13"?' open':'')}>
-                { this.props.number }
+            <div className={'Door'+(this.props.number==="24"?' double':'')+(this.state.open===true?' open':'')} onClick={ this.onClick }>
+                <div className="front">
+                    { this.props.number }
+                </div>
+                <div className="back">Merry Christmas</div>
+                <div className="window">Yo</div>
             </div>
         );
     }
