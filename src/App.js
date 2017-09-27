c218cafcc93afbfb1a6653f0fc26e74519ce28b0
Chris Neale <chris@komododigital.co.uk>
2017-09-27 09:07:55 +0100

    Fixed layout issues

--- src/App.js
+++ src/App.js
@@ -4,36 +4,54 @@ import './App.css';
 import Door from './Door.js';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="Advent">
-          <Door number="01" image="https://source.unsplash.com/NzQknDofRpc"></Door>
-          <Door number="02" image="https://source.unsplash.com/z0nZ229ifFw"></Door>
-          <Door number="03" image="https://source.unsplash.com/mXvgSOsnPx0"></Door>
-          <Door number="04" image="https://source.unsplash.com/xLGtGvH0A8g"></Door>
-          <Door number="05" image="https://source.unsplash.com/r53rNKb_7s8"></Door>
-          <Door number="06" image="https://source.unsplash.com/WsDF95mSUsI"></Door>
-          <Door number="07" image="https://source.unsplash.com/iZTMUjrvX4c"></Door>
-          <Door number="08" image="https://source.unsplash.com/u2n4LFarEMI"></Door>
-          <Door number="09" image="https://source.unsplash.com/7AzZqUXYCac"></Door>
-          <Door number="10" image="https://source.unsplash.com/VfWkdMue5Jc"></Door>
-          <Door number="11" image="https://source.unsplash.com/dABKxsPTAEk"></Door>
-          <Door number="12" image="https://source.unsplash.com/NizoYtO0yiY"></Door>
-          <Door number="13" image="https://source.unsplash.com/FQ5N5gb9Cao"></Door>
-          <Door number="14" image="https://source.unsplash.com/vJz7tkHncFk"></Door>
-          <Door number="15" image="https://source.unsplash.com/JsjXnWlh8-g"></Door>
-          <Door number="16" image="https://source.unsplash.com/G8ooPghadAY"></Door>
-          <Door number="17" image="https://source.unsplash.com/rtMiBkMCOsw"></Door>
-          <Door number="18" image="https://source.unsplash.com/Gork0BACw2I"></Door>
-          <Door number="19" image="https://source.unsplash.com/k0SwnevO_wk"></Door>
-          <Door number="20" image="https://source.unsplash.com/2gSfZ9Baph8"></Door>
-          <Door number="21" image="https://source.unsplash.com/UIlHiyFy0Wk"></Door>
-          <Door number="22" image="https://source.unsplash.com/GAJ4g8f7FBk"></Door>
-          <Door number="23" image="https://source.unsplash.com/O1gHgEagQ9U"></Door>
-          <Door number="24" image="https://source.unsplash.com/SSxIGsySh8o"></Door>
-      </div>
-    );
-  }
+
+    constructor() {
+        super();
+        this.state = {
+            doors: [
+                {number:"01", image:"https://source.unsplash.com/NzQknDofRpc"},
+                {number:"02", image:"https://source.unsplash.com/z0nZ229ifFw"},
+                {number:"03", image:"https://source.unsplash.com/mXvgSOsnPx0"},
+                {number:"04", image:"https://source.unsplash.com/xLGtGvH0A8g"},
+                {number:"05", image:"https://source.unsplash.com/r53rNKb_7s8"},
+                {number:"06", image:"https://source.unsplash.com/WsDF95mSUsI"},
+                {number:"07", image:"https://source.unsplash.com/iZTMUjrvX4c"},
+                {number:"08", image:"https://source.unsplash.com/u2n4LFarEMI"},
+                {number:"09", image:"https://source.unsplash.com/7AzZqUXYCac"},
+                {number:"10", image:"https://source.unsplash.com/VfWkdMue5Jc"},
+                {number:"11", image:"https://source.unsplash.com/dABKxsPTAEk"},
+                {number:"12", image:"https://source.unsplash.com/NizoYtO0yiY"},
+                {number:"13", image:"https://source.unsplash.com/FQ5N5gb9Cao"},
+                {number:"14", image:"https://source.unsplash.com/vJz7tkHncFk"},
+                {number:"15", image:"https://source.unsplash.com/JsjXnWlh8-g"},
+                {number:"16", image:"https://source.unsplash.com/G8ooPghadAY"},
+                {number:"17", image:"https://source.unsplash.com/rtMiBkMCOsw"},
+                {number:"18", image:"https://source.unsplash.com/Gork0BACw2I"},
+                {number:"19", image:"https://source.unsplash.com/k0SwnevO_wk"},
+                {number:"20", image:"https://source.unsplash.com/2gSfZ9Baph8"},
+                {number:"21", image:"https://source.unsplash.com/UIlHiyFy0Wk"},
+                {number:"22", image:"https://source.unsplash.com/GAJ4g8f7FBk"},
+                {number:"23", image:"https://source.unsplash.com/O1gHgEagQ9U"},
+                {number:"24", image:"https://source.unsplash.com/SSxIGsySh8o"}
+            ]
+        }
+
+    }
+    render() {
+
+        let _doors = this.state.doors;
+        //    _doors.sort(function(door) { return 0.5 - Math.random() });
+
+        let doors = _doors.map((door)=>{
+            return <Door key={door.number} number={door.number} image={door.image}></Door>
+        })
+
+        return (
+            <div className="Advent">
+                {doors}
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Door.js
+++ src/Door.js
@@ -6,26 +6,33 @@ class Door extends Component {
 
     constructor(){
         super();
-        this.state = { open: false, backgroundPosition: "0px 0px" };
+        this.state = { open: false, animating: false,  backgroundPosition: "0px 0px" };
         this.onClick = this.onClick.bind(this);
+        this.animating = this.animating.bind(this);
     }
 
     componentDidMount(){
-        
+
         var el = ReactDOM.findDOMNode(this).getBoundingClientRect();
         this.setState({ backgroundPosition: (-1*el.left)+"px "+(-1*el.top)+"px" });
-    
+        this.refs.door.addEventListener('transitionstart', this.animating);
+        this.refs.door.addEventListener('transitionend', this.animating);
+        
+    }
+
+    animating () {
+        this.setState({animating: !this.state.animating});
     }
 
     onClick(){
-        this.setState({ open: !this.state.open });
+        this.setState({ open: !this.state.open, animating: !this.state.animating });
     }
 
     render() {
 
         return (
-            <div className={'Window'+(this.state.open===true?' opened':'')+(this.props.number==="24"?' double':'')}>
-                <div className={'Door'+(this.state.open===true?' open':'')} onClick={ this.onClick } style={{ backgroundPosition: this.state.backgroundPosition }}>
+            <div className={'Window'+(this.state.open===true?' opened':'')+(this.state.animating===true?' animating':'')+(this.props.number==="24"?' double':'')}>
+                <div ref="door" className={'Door'+(this.state.open===true?' open':'')} onClick={ this.onClick } style={{ backgroundPosition: this.state.backgroundPosition }}>
                     <div className="front">
                         { this.props.number }
                     </div>
