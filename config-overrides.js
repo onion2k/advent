2eab5bc9bfe46b0930ff8dff326dda50cdb2f415
Chris Neale <chris@komododigital.co.uk>
2017-10-06 08:40:27 +0100

    Added comment to config

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,14 @@
+/*
+
+Using react-app-rewired to get webpack-bundle-analyzer working with create-react-app without needing to eject, because CRA is nice and all but sometimes it's good to see under the bonnet.
+
+Use `npm install -D react-app-rewired webpack-bundle-analyzer` to get this to work.
+
+*/
+
 var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = (config, env) => {
     config.plugins.push(new BundleAnalyzerPlugin());
     return config;
-  }
+}
